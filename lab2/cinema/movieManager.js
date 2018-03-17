c582ed1997ae6db8934c1f0d0aa2a42ea2629557
implssv <implossive@gmail.com>
2018-03-17 09:51:18 +0100

    fixes

--- lab2/cinema/movieManager.js
+++ lab2/cinema/movieManager.js
@@ -1,4 +1,8 @@
-var movieManager = (function () {
+"use strict";
+var lib = {};
+
+
+lib.movieManager = (function () {
 
 
     var movies = [
@@ -6,38 +10,33 @@ var movieManager = (function () {
 
         {
             id: 1,
-            title: 'The Shawshank Redemption',
+            title: "The Shawshank Redemption",
             productionYear: 1994,
             rating: 9
 
         },
         {
             id: 2,
-            title: 'Forrest Gump',
+            title: "Forrest Gump",
             productionYear: 1994,
             rating: 9
 
         },
         {
             id: 3,
-            title: 'Red Sparrow',
+            title: "Red Sparrow",
             productionYear: 2018,
             rating: 7
 
         },
         {
             id: 4,
-            title: 'The Silence of the Lambs',
+            title: "The Silence of the Lambs",
             productionYear: 1991,
             rating: 8
+        }];
 
-        },
-
-
-
-
-    ],
-        interface =
+       var publicInterface =
             {
 
                 getAllMovies: function () {
@@ -49,76 +48,36 @@ var movieManager = (function () {
                 getMovieById: function (id) {
 
 
-
-                    for (var i = 0; i < movies.length; i++) {
-
-                        if (movies[i].id === id)
-                            return movies[i];
-                    }
+                 return movies.filter((movie) => movie.id===id);
 
                 },
 
-                getMoviesByProductionYear: function (year) {
+                getMoviesByProductionYear: function (productionYear) {
 
-                    var filteredMovies = [];
-
-                    movies.forEach(function (movie) {
-
-
-                        if (movie.productionYear === year)
-                            filteredMovies.push(movie);
-
-                    })
-
-                    return filteredMovies;
+                   return movies.filter((movie) => movie.productionYear===productionYear);
 
                 },
 
                 getMoviesByRating: function (rating) {
-
-                    var filteredMovies = [];
-
-                    movies.forEach(function (movie) {
-
-
-                        if (movie.rating === rating)
-                            filteredMovies.push(movie);
-
-                    })
-
-                    return filteredMovies;
+                    
+                   return movies.filter((movie) => movie.rating === rating);
 
                 },
 
                 getMoviesWithRatingGreaterThan: function (rating) {
 
 
-                    var filteredMovies = [];
-
-                    movies.forEach(function (movie) {
-
-
-                        if (movie.rating >= rating)
-                            filteredMovies.push(movie);
-
-                    })
-
-                    return filteredMovies;
+                    return movies.filter((movie) => movie.rating >= rating);
 
                 }
 
+            };
 
 
+    return publicInterface;
 
 
-
-            }
-
-
-    return interface;
-
-
-})();
+}());
 
 
 
@@ -131,6 +90,6 @@ var movieManager = (function () {
 
 // console.log(movieManager.getMoviesByRating(8));
 
-console.log(movieManager.getMoviesWithRatingGreaterThan(8));
+//console.log(lib.movieManager.getMoviesWithRatingGreaterThan(8));
 
 
